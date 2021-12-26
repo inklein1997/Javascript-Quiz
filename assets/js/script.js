049f841d0bd87a8017155109d77692ddcb705542
Michael Klein <Michaelklein1997@gmail.com>
2021-12-26 16:27:32 -0600

    added timer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,20 +7,23 @@ var choice2El = document.createElement("button");
 var choice3El = document.createElement("button");
 var choice4El = document.createElement("button");
 
+var timeLeft
+
 
 startEl.addEventListener("click", start)
 
 function start() {
     mainEl.remove();
+    startTimer();
 }
 
-
-
-
-//click start
-    //1. timer starts
-    //2. screen clears
-    //3. first question pops up
-        //1. question pops up up with 4 buttons
-        //2. user must select button
-        //3. if user selects wrong button, timer decreases by 10 and 
+function startTimer () {
+    timeLeft=100
+    var timeInterval = setInterval(function() {
+        timeLeft--;
+        timerEl.textContent = "Time Left: " + timeLeft;
+        if (timeLeft == 0) {
+            clearInterval(timeInterval)
+        }
+    }, 1000)
+}
