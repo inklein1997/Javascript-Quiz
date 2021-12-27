5e8e073d297208266c7749f6f8838b29ba8c2178
Michael Klein <Michaelklein1997@gmail.com>
2021-12-27 15:48:26 -0600

    randomized the appending of buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,11 +30,19 @@ var timeLeft
 var questions = ["adds new elements to the end of an array, and returns the new length", "Joins all elements of an array into a string","Selects a part of an array, and returns the new array","Creates a new array with the result of calling a function for each array element","Removes the last element of an array, and returns that element"];
 var answers;
 
+
+
 var question
+var position
 var answer1
 var answer2
 var answer3
 var correctAnswer
+
+//Remaining things to do...
+    //highscore storage
+    //event.preventDefault();
+    //randomize choice layout
 
 startEl.addEventListener("click", start);
 correctChoiceEl.addEventListener("click", notifyCorrect);   //if clicked, adds 1 to score and notifies user answer is correct.  generates new question
@@ -59,8 +67,8 @@ function startTimer () {
     timeLeft=100
     var timeInterval = setInterval(function() {
         timeLeft-=0.01;
-        timerEl.textContent = "Time Left: " + timeLeft.toFixed(2);
-        if ((timeLeft <= 0) || questions.length == 0) {
+        timerEl.textContent = "Time Left: " + timeLeft.toFixed(2);      //displays countdown
+        if ((timeLeft <= 0) || questions.length == 0) {                 //If user runs out of time or clicks through all the answer, the quiz ends.
             clearInterval(timeInterval);
             newMainEl.remove();
             quizEnd();
@@ -72,8 +80,8 @@ function pullQuestionAndAnswer() {
     var answers = ["push()", "join()", "slice()", "map()", "pop()"];
 
     question = questions[Math.floor(Math.random()*questions.length)];   //selects random question from array
-    var position = questions.indexOf(question);                         //determines position of question
-    questions.splice(position, 1);                                      //removes question from array so it cannot be asked again
+    position = questions.indexOf(question);                             //determines position of question
+    
     
     correctAnswer = answers[position];                                  //answers array position matches with questions array position.  Will always be the correct answer.
     answers.splice(position,1);                                         //removes option so the answer cannot be selected again for question.
@@ -85,19 +93,24 @@ function pullQuestionAndAnswer() {
     answers.splice(answers.indexOf(answer2),1);
 
     answer3 = answers[Math.floor(Math.random()*answers.length)];
-    answers.splice(answers.indexOf(answer3),1);
-
+    answers.splice(answers.indexOf(answer3),1); 
 }
 
+
 function generateQuestionDisplay() {
+    var appendOption = [correctChoiceEl, choice1El, choice2El, choice3El]
+    
+    for (var i = 0; i < 4; i++) {       //For loop appends options in randomized order
+    var randomAppendOption = appendOption[Math.floor(Math.random()*appendOption.length)]
+    var positionAppendOption = appendOption.indexOf(randomAppendOption);
+    appendOption.splice(positionAppendOption, 1);
+    divEl2.appendChild(randomAppendOption);
+    }
+
     bodyEl.appendChild(newMainEl)
     newMainEl.appendChild(divEl1)
     divEl1.appendChild(questionEl);
     newMainEl.appendChild(divEl2);
-    divEl2.appendChild(choice1El);
-    divEl2.appendChild(choice2El);
-    divEl2.appendChild(choice3El);
-    divEl2.appendChild(correctChoiceEl);
     newMainEl.appendChild(notifyEl);
 
     divEl1.setAttribute("class", "row")
@@ -120,11 +133,13 @@ function askQuestion() {
 
 function notifyCorrect() {
     notifyEl.textContent = "You answered correctly!";
+    questions.splice(position, 1);      //removes question from array so it cannot be asked again
     askAgain();
 }
 
 function notifyIncorrect() {
     notifyEl.textContent = "You answered incorrectly!";
+    questions.splice(position, 1);      //removes question from array so it cannot be asked again
     timeLeft -= 10;
     askAgain();
 }
